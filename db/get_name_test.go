223307e198602c734ba8b25636167e77b79b94df
Mark <markzheng1130@gmail.com>
2024-03-19 16:28:28 +0800

    add

--- db/get_name_test.go
+++ db/get_name_test.go
@@ -12,17 +12,40 @@ func TestGetName(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	m := NewMockDB(ctrl)
+	db := NewMockDB(ctrl)
 
-	m.EXPECT().
+	db.EXPECT().
 		GetNameByIndex(gomock.Eq(1)).
 		DoAndReturn(func(_ int) string {
 			time.Sleep(1000)
 			return "cat"
 		})
 
-	m.EXPECT().
+	db.EXPECT().
 		GetNameByIndex(gomock.Eq(2)).
 		Return("dog")
 
+	tests := []struct {
+		name string
+		arg  int
+		want string
+	}{
+		{
+			"test-case-1",
+			1,
+			"cat",
+		},
+		{
+			"test-case-2",
+			2,
+			"dog",
+		},
+	}
+
+	for _, test := range tests {
+		Convey(test.name, t, func() {
+			name := GetName(db, test.arg)
+			So(name, ShouldEqual, test.want)
+		})
+	}
 }
