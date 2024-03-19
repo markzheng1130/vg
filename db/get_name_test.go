a5fd5d15076a6a53107918b66f228382a8b4e6a8
Mark <markzheng1130@gmail.com>
2024-03-19 16:24:34 +0800

    add

--- db/get_name_test.go
+++ db/get_name_test.go
@@ -2,12 +2,27 @@ package db
 
 import (
 	"testing"
+	"time"
 
-	"go.uber.org/mock/gomock"
+	"github.com/golang/mock/gomock"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestGetName(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
+
+	m := NewMockDB(ctrl)
+
+	m.EXPECT().
+		GetNameByIndex(gomock.Eq(1)).
+		DoAndReturn(func(_ int) string {
+			time.Sleep(1000)
+			return "cat"
+		})
+
+	m.EXPECT().
+		GetNameByIndex(gomock.Eq(2)).
+		Return("dog")
 
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,11 @@ require (
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.6.0
-	go.uber.org/mock v0.4.0
+	github.com/smartystreets/goconvey v1.8.1
+)
+
+require (
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/smarty/assertions v1.15.0 // indirect
 )
