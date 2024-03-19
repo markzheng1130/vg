8ac8693fe490c0248a62d35ae644c3cabe39e216
Mark <markzheng1130@gmail.com>
2024-03-19 15:40:11 +0800

    add

--- db/get_name.go
+++ db/get_name.go
@@ -8,4 +8,4 @@ func GetName(db DB, index int) string {
 	return db.GetNameByIndex(index)
 }
 
-// mockgen -destination get_name_mock.go -package mock_db -source get_name.go
+// mockgen -destination get_name_mock.go -package db -source get_name.go
