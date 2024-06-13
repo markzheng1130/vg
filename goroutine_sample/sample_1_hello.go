fd4d4747d3a7e45cd7eebb60306b57a0b22ccc33
mark_cheng <mark_cheng@trendmicro.com>
2024-06-13 19:48:20 +0800

    add

--- goroutine_sample/sample_1_hello.go
+++ goroutine_sample/sample_1_hello.go
@@ -10,11 +10,14 @@ func hello() {
 }
 
 func main() {
-
 	go hello()
+
+	fmt.Printf("[main][start]\n")
 
 	for countDown := 3; countDown > 0; countDown-- {
 		fmt.Printf("[countDown][%v]\n", countDown)
 		time.Sleep(time.Second * 1)
 	}
+
+	fmt.Printf("[main][end]\n")
 }
