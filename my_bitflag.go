b609e1a7b7493d1cd2743e1769cb04d967750dae
Mark Cheng <mark_cheng@trendmicro.com>
2024-12-31 17:49:54 +0800

    v1e-36170-fix-typo-space

--- my_bitflag.go
+++ my_bitflag.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var CreditAllocations int32
-	CreditAllocations = 15
-	creditAllocationStatus := ExtractCreditAllocationStatus(CreditAllocations)
+	for CreditAllocations := int32(0); CreditAllocations < 16; CreditAllocations++ {
+		CreditAllocationStatus := ExtractCreditAllocationStatus(CreditAllocations)
 
-	for _, v := range creditAllocationStatus {
-		fmt.Printf("[%v]\n", v)
+		fmt.Printf("[%v]\n", CreditAllocations)
+		for _, v := range CreditAllocationStatus {
+			fmt.Printf("[%v]\n", v)
+		}
+		fmt.Printf("\n\n")
 	}
-
 }
 
 func ExtractCreditAllocationStatus(CreditAllocations int32) []string {
@@ -36,7 +37,7 @@ func extractBit(num int32, position uint) int32 {
 	return (num >> position) & 1 // Extract the bit (0 or 1) from the position.
 }
 
-var creditAllocationBitMapping = map[int]creditAllocation{
+var creditAllocationBitMapping = map[int32]creditAllocation{
 	0: endpointSensorDetectionAndResponse,
 	1: advancedEndpointSecurity,
 	2: advancedServerAndWorkloadProtection,
@@ -46,10 +47,10 @@ var creditAllocationBitMapping = map[int]creditAllocation{
 type creditAllocation string
 
 const ( // Should maintain wording with HIE figma: https://www.figma.com/design/Qc88PAh9wdodfzdUwV5F82/Server-%26-Workload-Protection?node-id=4871-18491&t=7ZS9JJIrCiPsoqHQ-0
-	endpointSensorDetectionAndResponse        creditAllocation = "Endpoint sensor detection and response"
-	advancedEndpointSecurity                  creditAllocation = "Advanced Endpoint Security"
-	advancedServerAndWorkloadProtection       creditAllocation = "Advanced Server & Workload Protection"
-	sapScannerForVisionOneEndpointSecurityPro creditAllocation = "SAP Scanner for Vision One - Endpoint Security (Pro)"
+	endpointSensorDetectionAndResponse        creditAllocation = "Endpoint sensor detection and response (1)" // [TODO]: remove brackets
+	advancedEndpointSecurity                  creditAllocation = "Advanced Endpoint Security (2)"
+	advancedServerAndWorkloadProtection       creditAllocation = "Advanced Server & Workload Protection (4)"
+	sapScannerForVisionOneEndpointSecurityPro creditAllocation = "SAP Scanner for Vision One - Endpoint Security (Pro) (8)"
 )
 
 func (c creditAllocation) String() string {
